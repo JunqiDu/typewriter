d4f474fb53a058eaf1f0a03e72c3d7482885dac3
JunqiDu <50341989+JunqiDu@users.noreply.github.com>
2023-01-17 12:55:49 -0500

    Add files via upload

---
+++ hello_timeout.js
@@ -0,0 +1,13 @@
+//challenge 1
+setTimeout(() => {
+  console.log('4311o th3r3 w0r1d');
+}, 3000);
+
+//challenge 2
+let array = ['4311o', 'th3r3', 'w0r1d'];
+for (let i = 0; i < array.length; i++) {
+  setTimeout(() => {
+    console.log(array[i]);
+  }, 3000);
+}
+
